457d45eec70cef8a4c269cc796dc4cba056f7008
makarchuk92 <andrei.makarchuk@mail.ru>
2022-10-14 00:34:15 +0300

    Created store, api(github)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Navigation = () => {
   return (
     <nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-slate-800 text-white' >
-      <h3>Github</h3>
+      <h3 className='font-bold'>Github</h3>
       <span>
         <Link to="/" className='mr-2'>Home</Link>
         <Link to="/favourites" className='mr-2'>Favourites</Link>
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSearchUsersQuery } from '../redux/github/github.api'
 
 const HomePage = () => {
+  const {isLoading, isError, data} = useSearchUsersQuery('makarchuk92')
+
   return (
     <div>HomePage</div>
   )
